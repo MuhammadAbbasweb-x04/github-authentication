b472f313df70f12cc17fcc1012481703da285045
MuhammadAbbasweb-x04 <mabbasmshahi2003@gmail.com>
2025-10-28 10:41:44 -0700

    100% done

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-import {signInWithPopup, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import {signInWithPopup, GithubAuthProvider ,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { auth, githubAuthProvider} from "/config.js";
 
+let btn = document.querySelector(".gh-btn");
 
-let btn = document.querySelector(".gh-btn")
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    console.log("user loggdin uid" , uid );
+  } else {
+window.location = "home.html";
+  }
+});
 btn.addEventListener("click", ()=>{
 signInWithPopup(auth, githubAuthProvider)
   .then((result) => {
