a1cbbf44851fd6c52a77c920fc1d21ca07501645
MuhammadAbbasweb-x04 <mabbasmshahi2003@gmail.com>
2025-11-01 11:04:11 -0700

    100% done

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-import {signInWithPopup, GithubAuthProvider ,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import {signInWithPopup, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { auth, githubAuthProvider} from "/config.js";
 
 let btn = document.querySelector(".gh-btn");
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    const uid = user.uid;
-    console.log("user loggdin uid" , uid );
-  } else {
-window.location = "home.html";
-  }
-});
-btn.addEventListener("click", ()=>{
+btn.addEventListener("click", (event)=>{
+  event.preventDefault();
 signInWithPopup(auth, githubAuthProvider)
   .then((result) => {
     const credential = GithubAuthProvider.credentialFromResult(result);
--- logout.js
+++ logout.js
@@ -1,6 +1,29 @@
+import {signOut,
+onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { auth } from "/config.js";
 let logout = document.querySelector(".logout-btn");
-logout.addEventListener("click" , ()=>{
-window.location = "index.html"
 
-})
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    console.log(`User uid : ${uid}`);   
+  }
+else{
+window.location = "index.html";
+}
+});
+
+logout.addEventListener("click" , () =>{
+signOut(auth)
+.then(() => {
+window.location = "index.html"
+console.log("response");
+
+}).catch((error) => {
+console.log(error, "error");
+
+}); 
+} )
+
 
