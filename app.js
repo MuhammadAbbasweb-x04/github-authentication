90e8ac280ef51e4660117b27cc01c02645256c61
MuhammadAbbas-coder004 <mabbasmshahi2003@gmail.com>
2025-11-09 22:51:09 +0500

    100% done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import {signInWithPopup, 
 GithubAuthProvider } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-import { auth, githubAuthProvider} from "/auth.js";
+import { auth, githubAuthProvider} from "./auth.js";
 
 let btn = document.querySelector(".gh-btn");
 btn.addEventListener("click", (event)=>{
